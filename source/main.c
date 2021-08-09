07d9006a6aa9c2bfef5b7bd58e562610b70a7f7a
Duckfeld-McJimmers <ivan.desc@gmail.com>
2021-08-09 12:29:36 -0700

    NEW

--- source/main.c
+++ source/main.c
@@ -1,138 +1,109 @@
+#include <grrlib.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <gccore.h>
-#include <string.h>
-#include <jpeg/jpgogc.h>
 #include <wiiuse/wpad.h>
-#include <unistd.h>
+#include "gfx/wii_jpg.h"
+	
+// RGBA Colors
+#define GRRLIB_BLACK   0x000000FF
+#define GRRLIB_MAROON  0x800000FF
+#define GRRLIB_GREEN   0x008000FF
+#define GRRLIB_OLIVE   0x808000FF
+#define GRRLIB_NAVY    0x000080FF
+#define GRRLIB_PURPLE  0x800080FF
+#define GRRLIB_TEAL    0x008080FF
+#define GRRLIB_GRAY    0x808080FF
+#define GRRLIB_SILVER  0xC0C0C0FF
+#define GRRLIB_RED     0xFF0000FF
+#define GRRLIB_LIME    0x00FF00FF
+#define GRRLIB_YELLOW  0xFFFF00FF
+#define GRRLIB_BLUE    0x0000FFFF
+#define GRRLIB_FUCHSIA 0xFF00FFFF
+#define GRRLIB_AQUA    0x00FFFFFF
+#define GRRLIB_WHITE   0xFFFFFFFF
 
 
-
-static u32 *xfb;
-static GXRModeObj *rmode;
-extern char picdata[];
-extern int  piclength;
-//-------------------------------------------------
-
-
-void Initialize() {
-
-	// Initialise the video system
-	VIDEO_Init();
-
-	// This function initialises the attached controllers
-	WPAD_Init();
-
-	switch(VIDEO_GetCurrentTvMode()) {
-		case VI_NTSC:
-			rmode = &TVNtsc480IntDf;
-			break;
-		case VI_PAL:
-			rmode = &TVPal528IntDf;
-			break;
-		case VI_MPAL:
-			rmode = &TVMpal480IntDf;
-			break;
-		default:
-			rmode = &TVNtsc480IntDf;
-			break;
+int colours[] = {
+	GRRLIB_MAROON,
+	GRRLIB_GREEN,
+	GRRLIB_OLIVE, 
+	GRRLIB_NAVY,    
+	GRRLIB_PURPLE,  
+	GRRLIB_TEAL,   
+	GRRLIB_GRAY,    
+	GRRLIB_SILVER,  
+	GRRLIB_RED,     
+	GRRLIB_LIME,    
+	GRRLIB_YELLOW,  
+	GRRLIB_BLUE,    
+	GRRLIB_FUCHSIA, 
+	GRRLIB_AQUA,    
+	GRRLIB_WHITE,
+};
+int x=0;
+void newColour(){
+	if (x==14){
+		x=0;
 	}
-
-	// Obtain the preferred video mode from the system
-	// This will correspond to the settings in the Wii menu
-	//rmode = VIDEO_GetPreferredMode(NULL);
-
-	// Allocate memory for the display in the uncached region
-	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-
-	// Initialise the console, required for printf
-	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
-
-	// Set up the video registers with the chosen mode
-	VIDEO_Configure(rmode);
-
-	// Tell the video hardware where our display memory is
-	VIDEO_SetNextFramebuffer(xfb);
-
-	// Make the display visible
-	VIDEO_SetBlack(FALSE);
-
-	// Flush the video register changes to the hardware
-	VIDEO_Flush();
-
-	// Wait for Video setup to complete
-	VIDEO_WaitVSync();
-	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
-
+	x++;
 }
 
 
-
-void display_jpeg(JPEGIMG jpeg, int x, int y) {
-	unsigned int *jpegout = (unsigned int *) jpeg.outbuffer;
-	int i,j;
-	int height = jpeg.height;
-	int width = jpeg.width/2;
-	for(i=0;i<=width;i++)
-	for(j=0;j<=height-2;j++)
-	xfb[(i+x)+320*(j+16+y)]=jpegout[i+width*j];
-	free(jpeg.outbuffer);
-}
-
-//---------------------------------------------------------------------------------
-int main() {
-//---------------------------------------------------------------------------------
-	
-	JPEGIMG wii;
-	memset(&wii, 0, sizeof(JPEGIMG));
-	wii.inbuffer = picdata;
-	wii.inbufferlength = piclength;
-	JPEG_Decompress(&wii);
-	
-	
-	Initialize();
-	
-	
-	
-	int updown=2;
-	int leftright=2;
-	int movelr=0;
-	int moveud=0;
-	
-	while(1) {
-		VIDEO_ClearFrameBuffer (rmode, xfb, COLOR_BLACK);
-		
-		// Call WPAD_ScanPads each loop, this reads the latest controller states
-		WPAD_ScanPads();
-
-		// WPAD_ButtonsDown tells us which buttons were pressed in this loop
-		// this is a "one shot" state which will not fire again until the button has been released
-		u32 pressed = WPAD_ButtonsDown(0);
-
-		// We return to the launcher application via exit
-		if ( pressed & WPAD_BUTTON_HOME ) exit(0);
-
-		// Wait for the next frame
-		
-		
-		movelr=movelr+leftright;
-		moveud=moveud+updown;
-		
-		 if ( moveud>420-wii.height) {
-			updown=updown*-1;
-		} else if (moveud<0){
-			updown=updown*-1;
-		}
-		
-		 if ( movelr>390-wii.width ){
-			leftright=leftright*-1;
-		} else if (movelr<0){
-			leftright=leftright*-1;
-		} 
-		
-		display_jpeg(wii, movelr, moveud);
-		VIDEO_WaitVSync();
-	}
-
-	return 0;
-}
+int main(int argc, char **argv) {
+    // Initialise the Graphics & Video subsystem
+    GRRLIB_Init();
+ 	
+    //load texture
+    GRRLIB_texImg *tex_wii_jpg = GRRLIB_LoadTexture(wii_jpg); 
+    
+    // Initialise the Wiimotes
+    WPAD_Init();
+    
+    int lr=1;
+    int ud=1;
+    int posx=0;
+    int posy=0;	
+ 
+    // Loop forever
+    while(1) {
+ 	
+        WPAD_ScanPads();  // Scan the Wiimotes
+ 
+        // If [HOME] was pressed on the first Wiimote, break out of the loop
+        if (WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME)  break;
+        // ---------------------------------------------------------------------
+        
+        
+        //move the logo
+        posx=posx+2*lr;
+        posy=posy+2*ud;
+        
+        
+        //collision detecting reverse direction.
+        if (posx<0 || posx>640-100) {
+        	lr=lr*-1;
+        	newColour();
+        }
+        
+        if (posy<0 || posy>480-44) {
+        	ud=ud*-1;
+        	newColour();
+        }
+        
+        
+        GRRLIB_DrawImg(posx, posy, tex_wii_jpg, 0, 1, 1, colours[x]);  // Draw a jpeg
+        
+        
+        
+        
+        
+        
+        // ---------------------------------------------------------------------
+ 
+        GRRLIB_Render();  // Render the frame buffer to the TV
+    }
+ 
+    GRRLIB_Exit(); // Be a good boy, clear the memory allocated by GRRLIB
+ 
+    exit(0);  // Use exit() to exit a program, do not use 'return' from main()
+   }
