3ec92eb0e55b4e1f6dfc37671c22d71aaa52108f
James Bennett <JamesBennett25565@gmail.com>
2021-12-20 21:55:41 +0000

    other stuff

--- source/main.c
+++ source/main.c
@@ -9,22 +9,22 @@
 #include "gfx/DVD_img.h"
 
 // RGBA Colours
-#define GRRLIB_BLACK 0x000000FF
-#define GRRLIB_MAROON 0x800000FF
-#define GRRLIB_GREEN 0x008000FF
-#define GRRLIB_OLIVE 0x808000FF
-#define GRRLIB_NAVY 0x000080FF
-#define GRRLIB_PURPLE 0x800080FF
-#define GRRLIB_TEAL 0x008080FF
-#define GRRLIB_GRAY 0x808080FF
-#define GRRLIB_SILVER 0xC0C0C0FF
-#define GRRLIB_RED 0xFF0000FF
-#define GRRLIB_LIME 0x00FF00FF
-#define GRRLIB_YELLOW 0xFFFF00FF
-#define GRRLIB_BLUE 0x0000FFFF
-#define GRRLIB_FUCHSIA 0xFF00FFFF
-#define GRRLIB_AQUA 0x00FFFFFF
-#define GRRLIB_WHITE 0xFFFFFFFF
+#define GRRLIB_BLACK    0x000000FF
+#define GRRLIB_MAROON   0x800000FF
+#define GRRLIB_GREEN    0x008000FF
+#define GRRLIB_OLIVE    0x808000FF
+#define GRRLIB_NAVY     0x000080FF
+#define GRRLIB_PURPLE   0x800080FF
+#define GRRLIB_TEAL     0x008080FF
+#define GRRLIB_GRAY     0x808080FF
+#define GRRLIB_SILVER   0xC0C0C0FF
+#define GRRLIB_RED      0xFF0000FF
+#define GRRLIB_LIME     0x00FF00FF
+#define GRRLIB_YELLOW   0xFFFF00FF
+#define GRRLIB_BLUE     0x0000FFFF
+#define GRRLIB_FUCHSIA  0xFF00FFFF
+#define GRRLIB_AQUA     0x00FFFFFF
+#define GRRLIB_WHITE    0xFFFFFFFF
 
 #define SYS_RETURNTOMENU 3  // Directly load the Wii Channels menu, without actually cold-resetting the system
 #define SYS_POWEROFF 4	    // Powers off the Wii, automatically choosing Standby or Idle mode depending on the user's configuration
